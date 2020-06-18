a19ba866d3b0ca6890f5271811e511d7c142f88b
Hyung Kim <hjkmines@gmail.com>
2020-06-18 00:32:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,15 @@
-
+require 'pry'
 
 def reverse_each_word(string)
   
-  string 
+  converted_array = string.split(" ") 
+  binding.pry
+  converted_array.each do |x| 
+    x.reverse 
+  end 
+  
+  
+end 
 
 
 
